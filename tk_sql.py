4891500957aa4fe80f9e71c1c6f12dd55217f212
E-cyborg <149093609+E-cyborg@users.noreply.github.com>
2024-05-16 12:32:43 +0530

    Update tk_sql.py

--- tk_sql.py
+++ tk_sql.py
@@ -4,13 +4,13 @@ import mysql.connector as sql
 from mysql.connector import Error
 from tkinter import messagebox
 import webbrowser
+import pyttsx3
 
 class Main:
     def menu_bar(self):
         menu_bar = tk.Menu(root)
         file_menu = tk.Menu(menu_bar, tearoff=0)
 
-        # file_menu.add_command(label="Back", command=self.)
         file_menu.add_command(label="Help", command=self.help_of_self)
         menu_bar.add_cascade(label="Menu", menu=file_menu)
         root.config(menu=menu_bar)
@@ -86,20 +86,29 @@ class Main:
     def clear_the_screen(self):
         for item in root.winfo_children():
             item.destroy()
-
 class table:
     def clear_the_screen(self):
         for item in root.winfo_children():
             item.destroy()
-            self.second_window()
+        self.second_window()
+        try:
+            if code is not None:
+                self.code_text.insert("1.0", code) 
+        except NameError:
+            pass 
+
+        self.second_window()
+
     def second_window(self):
         main_class_instance = Main()
         main_class_instance.menu_bar()
         data_label = tk.Label(root, text="Database", font=("TkDefaultFont", 15))
         data_label.place(x=0, y=0)
+        code_lable=tk.Label(root,text='code here')
+        code_lable.place(x=150,y=30)
         cursor.execute('SHOW DATABASES')
         databases = cursor.fetchall()
-
+        global dbase
         dbase = Listbox(root)
         for index, db in enumerate(databases):
             dbase.insert(index, db[0])
@@ -107,6 +116,7 @@ class table:
         dbase.place(x=0, y=25)
         self.text_of_second_window()
         self.reload_button()
+
     def selected_database(self, event):
         selected_index = event.widget.curselection()
         if selected_index:
@@ -131,12 +141,10 @@ class table:
 
     def selected_table(self, event):
         selected_index = event.widget.curselection()
-        
         if selected_index:
             global selected_tb
             selected_tb = event.widget.get(selected_index[0])
         self.attributes_of_table()
-
 
     def attributes_of_table(self):
         colum_lable=tk.Label(text="Columns", font=("TkDefaultFont", 15))
@@ -151,21 +159,21 @@ class table:
             attribut.insert(index,col)
         attribut.place(x=0,y=450)
 
-
     def run_of_second_window(self):
         self.run_button = tk.Button(text="Run", command=self.getting_the_text)
         self.run_button.place(x=1400, y=0,width=120)
 
     def text_of_second_window(self):
+        global code_text
         self.code_text = Text(root, height=20, width=140, background="lightblue")
         self.code_text.place(x=150, y=50)
         self.run_of_second_window()
 
-
-         
     def getting_the_text(self):   
-        global code  
+        global code
         code = self.code_text.get("1.0", "end-1c")
+        if 'select' in code:
+            self.checking_the_select_command()
         self.running_the_input_code()
 
     def running_the_input_code(self):
@@ -178,26 +186,26 @@ class table:
             output_text_box.config(state='disabled')
         except Error as e:
             output_error=e
-            self.output_of_the_input_comm()
-        except NameError as e:
-            output_error=e
-            self.output_of_the_input_comm()
-    def output_of_the_input_comm(self):
+            self.output_of_the_input_comm(e)
+        except NameError:
+            output_error="Success"
+            self.output_of_the_input_comm(output_error)
+
+    def output_of_the_input_comm(self,e):
         global output_text_box
-        if 'output_text_box' not in globals():
-            output_text_box = tk.Text(root, height=10, width=140, background="lightyellow")
-            output_text_box.config(state='normal')
-            output_text_box.place(x=150, y=400)
+        # if 'output_text_box' not in globals():
+        output_text_box = tk.Text(root, height=10, width=140, background="lightyellow")
         output_text_box.config(state='normal')
-        output_text_box.insert(tk.END, str(output_error) + '\n')
+        output_text_box.place(x=150, y=400)
+        output_text_box.config(state='normal')
+        output_text_box.insert(tk.END, str(e) + '\n')
         output_text_box.config(state='disabled')
         self.clear_button_of_output_box()
 
-
     def clear_button_of_output_box(self):
-        
         clear_button=tk.Button(text='clear',command=self.clearing_output_box)
         clear_button.place(x=1400, y=30,width=120)
+
     def clearing_output_box(self):
         output_text_box.config(state='normal')
         output_text_box.delete("1.0", tk.END)
@@ -205,36 +213,36 @@ class table:
 
     def reload_button(self):
         global reload
-        reload=tk.Button(root,text="Reload",command=self.clear_the_screen)
-        reload.place(x=125,y=20)
-
+        reload=tk.Button(root,text="full Reload",command=self.clear_the_screen)
+        reload.place(x=1430,y=70)
+        
+    def checking_the_select_command(self):
+        if 'select' in code:
+            for line in code.split('\n'):
+                if line.strip().lower().startswith('select'):
+                    select = tk.Tk()
+                    try:
+                        cursor.execute(line)    
+                        result = cursor.fetchall()
+                        select_box = Text(select, height=20, width=150)
+                        if result:
+                            column_widths = [max(len(str(item)) for item in col) for col in zip(*result)]
+                            select_box.insert(END, ' | '.join([f'{col:<{width}}' for col, width in zip(column_names, column_widths)]))
+                            select_box.insert(END, '\n')
+                            for row in result:
+                                select_box.insert(END, ' | '.join([f'{str(item):<{width}}' for item, width in zip(row, column_widths)]))
+                                select_box.insert(END, '\n')
+                        else:
+                            select_box.insert(END, 'Table is empty')
+                        select_box.pack()
+                        select_box.config(state='disabled')
+                        select.mainloop()
+                    except Error as e:
+                        self.output_of_the_input_comm(e)
 
 if __name__ == "__main__":
+    announce = pyttsx3.init()
+    announce.say("This program is made by E cyborg. If you have any questions, ask me on GitHub.")
+    announce.runAndWait()
     a = Main()
     a.main()
-
-
-
-
-
-        # output_text_box.insert(END,x)
-            # messagebox.showerror('Error', str(e))
-
-
-
-
-
-
-    # def output_of_the_input_comm(self):
-    #     global output_text_box 
-    #     if 'output_text_box' in globals():
-    #         output_text_box.config(state='normal')
-    #         output_text_box.insert(tk.END, str(output_error) + '\n')
-    #         output_text_box.config(state='disabled')
-    #     else:
-    #         output_text_box = tk.Text(root, height=10, width=140, background="lightyellow")
-    #         output_text_box.config(state='normal')
-    #         output_text_box.place(x=150, y=400)
-    #         output_text_box.insert(tk.END, str(output_error) + '\n')
-    #         output_text_box.config(state='disabled')
-    #     self.clear_button_of_output_box()
